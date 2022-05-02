80588c8b7086a02418ecd3ae28864121dbbd0961
Ricardo González <rgr2009@hotmail.com>
2022-05-02 16:11:20 -0500

    calcukladora

--- calculadorafiscal.py
+++ calculadorafiscal.py
@@ -1,3 +1,19 @@
-#Calculadora Fiscal
+#Calculadora de ISR
 
-calculaiva="iva"
+from typing import final
+
+
+print("Bienvenidos a la calculadora de ISR")
+iva=16
+isr=1.25
+montoacalcular="salario"
+
+def calcular_isr(salario):
+        print=input("Ingresa el monto: ")
+        salario=salario*iva/100
+        
+        
+calcular_isr()
+
+
+
